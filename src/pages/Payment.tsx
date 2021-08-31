5726602012586e7f798a3566492ee6db3c33f2be
ziho <ziho@kakao.com>
2021-08-31 13:30:31 +0900

    [ADD] 앞으로 만들 페이지 생성

---
+++ src/pages/Payment.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Container = styled.div`
+  margin-top: 5rem;
+  display: flex;
+  flex-direction: column;
+`;
+
+function Payment() {
+  return <Container></Container>;
+}
+
+export default Payment;
