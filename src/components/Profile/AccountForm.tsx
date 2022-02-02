06ab17bf576a920d3ed9d2d55c40fad494649f7e
zih0 <ziho@kakao.com>
2022-02-02 15:01:44 +0900

    feat: 마이페이지 계좌번호 수정 추가

--- src/components/Profile/AccountForm.tsx
+++ src/components/Profile/AccountForm.tsx
@@ -1,7 +1,12 @@
+import { faSpinner } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React, { useContext, useState } from "react";
 import styled from "styled-components";
 import { AuthContext } from "../../contexts/AuthContext";
+import { API } from "../../firebase/api";
 import { bankList } from "../../utils/constants";
+import { isValidAccountLength, isValidNumber } from "../../utils/validation";
+import Button from "../Button";
 import Input from "../Input";
 import Select from "../Select";
 
@@ -21,7 +26,7 @@ const Form = styled.form`
 
     position: relative;
 
-    .nickname-error {
+    .account-error {
       position: absolute;
       bottom: -1.5rem;
       left: 0;
@@ -36,11 +41,6 @@ const Form = styled.form`
     margin-bottom: 0.5rem;
   }
 
-  .profile-nickname-wrapper {
-    display: flex;
-    gap: 1rem;
-  }
-
   .profile-form-input {
     width: 100%;
   }
@@ -49,11 +49,16 @@ const Form = styled.form`
 const StyledInput = styled(Input)`
   padding: 1rem;
 `;
+
+const StyledButton = styled(Button)`
+  width: 100%;
+  margin-top: 1rem;
+`;
+
 function AccountForm() {
   const { user, auth } = useContext(AuthContext);
   const [bank, setBank] = useState(user.bank ?? "");
   const [account, setAccount] = useState(user.account ?? "");
-
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState("");
 
@@ -69,11 +74,32 @@ function AccountForm() {
     const {
       currentTarget: { value },
     } = e;
+
+    setError("");
     setAccount(value);
   };
 
+  const onSubmitAccount = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    if (!isValidNumber(account)) {
+      setError("계좌번호에 숫자만 입력해주세요.");
+      return;
+    }
+    if (!isValidAccountLength(account)) {
+      setError("계좌번호를 다시 확인해주세요.");
+      return;
+    }
+    setLoading(true);
+
+    await API.setAccountData(auth?.uid, bank, account);
+    setLoading(false);
+
+    // TODO: 성공 토스트 추가
+  };
+
   return (
-    <Form>
+    <Form onSubmit={onSubmitAccount}>
       <div className="profile-input-wrapper">
         <p className="profile-form-label">은행</p>
         <Select
@@ -96,6 +122,14 @@ function AccountForm() {
           value={account}
           onChange={onChangeAccount}
         />
+        <StyledButton size="sm" type="submit">
+          {!loading ? (
+            "계좌번호 수정하기"
+          ) : (
+            <FontAwesomeIcon icon={faSpinner} spin={true} />
+          )}
+        </StyledButton>
+        {error && <span className="account-error">{error}</span>}
       </div>
     </Form>
   );
