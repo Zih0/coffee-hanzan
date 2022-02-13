c68a1b095a759de67d38422ca68346c99227c2ab
zih0 <ziho@kakao.com>
2022-02-13 18:12:35 +0900

    style: 소셜 링크 버튼 스타일 수정

--- src/components/Profile/SocialLink.tsx
+++ src/components/Profile/SocialLink.tsx
@@ -1,16 +1,29 @@
+import { faLink } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React from "react";
+import styled from "styled-components";
 import useModal from "../../hooks/useModal";
 import Button from "../Button";
 import SocialLinkModal from "../Modal/SocialLinkModal";
+
+const StyledButton = styled(Button)`
+  display: flex;
+  gap: 0.2rem;
+  font-size: 0.9rem;
+  letter-spacing: -0.05em;
+  background-color: ${({ theme }) => theme.color.white};
+  color: ${({ theme }) => theme.color.black};
+`;
 
 function SocialLink() {
   const { openModal, closeModal, ModalPortal } = useModal();
 
   return (
     <>
-      <Button size="sm" onClick={openModal}>
+      <StyledButton size="sm" onClick={openModal}>
+        <FontAwesomeIcon icon={faLink} />
         소셜 정보 연결
-      </Button>
+      </StyledButton>
       <ModalPortal>
         <SocialLinkModal onClose={closeModal} />
       </ModalPortal>
