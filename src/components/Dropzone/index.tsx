1d8c1fb601254da474de1e75fe20f8155dc07073
zih0 <ziho@kakao.com>
2022-02-17 00:30:56 +0900

    refactor: 이미지 크롭 분리

--- src/components/Dropzone/index.tsx
+++ src/components/Dropzone/index.tsx
@@ -1,42 +1,32 @@
-import React, { useCallback, useState } from "react";
+import React, { useCallback } from "react";
 import styled from "styled-components";
 import { faSpinner } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { useDropzone } from "react-dropzone";
-import ReactCrop, { Crop } from "react-image-crop";
-import "react-image-crop/dist/ReactCrop.css";
 
-function Dropzone() {
-  const [crop, setCrop] = useState<Crop>({
-    aspect: 1280 / 300,
-    x: 0,
-    y: 0,
-    width: 504,
-    height: 118,
-    unit: "px",
-  });
-  const [image, setImage] = useState("");
-  const onDrop = useCallback((file) => {
-    setImage(URL.createObjectURL(file[0]));
-  }, []);
+interface IDropzoneProps {
+  onChangeImage: (file: File) => void;
+}
+
+function Dropzone({ onChangeImage }: IDropzoneProps) {
+  const onDrop = useCallback(
+    (files: File[]) => {
+      onChangeImage(files[0]);
+    },
+    [onChangeImage]
+  );
+
   const { getRootProps, getInputProps, isDragActive } = useDropzone({ onDrop });
 
   return (
-    <>
-      <Container {...getRootProps()}>
-        <input {...getInputProps()} />
-        {isDragActive ? (
-          <FontAwesomeIcon icon={faSpinner} spin={true} />
-        ) : (
-          <p>이미지를 드래그하거나 클릭하여 첨부해주세요.</p>
-        )}
-      </Container>
-      <ReactCrop
-        src={image}
-        crop={crop}
-        onChange={(newCrop) => setCrop(newCrop)}
-      />
-    </>
+    <Container {...getRootProps()}>
+      <input {...getInputProps()} />
+      {isDragActive ? (
+        <FontAwesomeIcon icon={faSpinner} spin={true} />
+      ) : (
+        <p>이미지를 드래그하거나 클릭하여 첨부해주세요.</p>
+      )}
+    </Container>
   );
 }
 
