1629e297f9baa6a4d0da7552fb6d8468bf390c30
zih0 <ziho@kakao.com>
2022-02-27 18:27:05 +0900

    refactor: 쓸데없는 API 호출 제거

--- src/pages/Feed.tsx
+++ src/pages/Feed.tsx
@@ -2,23 +2,21 @@ import React, { useContext, useEffect } from "react";
 import { useHistory } from "react-router-dom";
 import styled from "styled-components";
 import { AuthContext } from "../contexts/AuthContext";
-import { API } from "../firebase/api";
 
 const Container = styled.main`
   display: flex;
   flex-direction: column;
   align-items: center;
+  width: 100%;
+  height: 90%;
 `;
 
 function Feed() {
-  const { user, setUser } = useContext(AuthContext);
+  const { user } = useContext(AuthContext);
   const history = useHistory();
 
   useEffect(() => {
-    API.getUserData(user.creatorId).then((fbUser) => {
-      if (!fbUser?.nickname) history.push("/setting");
-      else setUser(fbUser);
-    });
+    if (!user.nickname) history.push("/setting");
   }, []);
 
   return <Container></Container>;
