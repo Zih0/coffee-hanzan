0f4784296554dec0cb2ab3ddcc21bd925bc8ebdf
zih0 <ziho@kakao.com>
2022-03-22 20:09:42 +0900

    feat: Suspense Header 아래 감싸기

--- src/routes/Router.tsx
+++ src/routes/Router.tsx
@@ -1,31 +1,31 @@
-import React, { lazy, Suspense } from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import Header from "../components/Header";
-import PrivateRoute from "./PrivateRoute";
-const SignUp = lazy(() => import("../pages/SignUp"));
-const Home = lazy(() => import("../pages/Home"));
-const Setting = lazy(() => import("../pages/Setting"));
-const Payment = lazy(() => import("../pages/Payment"));
-const Feed = lazy(() => import("../pages/Feed"));
-const Profile = lazy(() => import("../pages/Profile"));
+import React, { lazy, Suspense } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Header from '../components/Header';
+import PrivateRoute from './PrivateRoute';
+const SignUp = lazy(() => import('../pages/SignUp'));
+const Home = lazy(() => import('../pages/Home'));
+const Setting = lazy(() => import('../pages/Setting'));
+const Payment = lazy(() => import('../pages/Payment'));
+const Feed = lazy(() => import('../pages/Feed'));
+const Profile = lazy(() => import('../pages/Profile'));
 
 function AppRouter() {
   return (
-    <Suspense fallback={<></>}>
-      <Router>
-        <Header />
+    <Router>
+      <Header />
+      <Suspense fallback={<></>}>
         <Switch>
-          <Route exact path="/" component={Home} />
-          <Route exact path="/signup" component={SignUp} />
+          <Route exact path='/' component={Home} />
+          <Route exact path='/signup' component={SignUp} />
 
-          <PrivateRoute exact path="/setting" component={Setting} />
-          <PrivateRoute exact path="/profile" component={Profile} />
-          <PrivateRoute exact path="/set-payment" component={Payment} />
+          <PrivateRoute exact path='/setting' component={Setting} />
+          <PrivateRoute exact path='/profile' component={Profile} />
+          <PrivateRoute exact path='/set-payment' component={Payment} />
 
-          <Route path="/:nickname" component={Feed} />
+          <Route path='/:nickname' component={Feed} />
         </Switch>
-      </Router>
-    </Suspense>
+      </Suspense>
+    </Router>
   );
 }
 
