0b45762b86cf5948f9290d56223875d28bf51d6f
zih0 <ziho@kakao.com>
2022-03-27 13:50:51 +0900

    feat: firebase app 변경

--- src/env.d.ts
+++ src/env.d.ts
@@ -3,6 +3,13 @@ interface ImportMetaEnv {
     readonly VITE_B: string;
     readonly VITE_C: string;
     readonly VITE_CRYPTO_KEY: string;
+    readonly VITE_FIREBASE_API_KEY: string;
+    readonly VITE_FIREBASE_AUTH_DOMAIN: string;
+    readonly VITE_FIREBASE_PROJECT_ID: string;
+    readonly VITE_FIREBASE_BUCKET: string;
+    readonly VITE_FIREBASE_SENDER_ID: string;
+    readonly VITE_FIREBASE_APP_ID: string;
+    readonly VITE_FIREBASE_MEASUREMENT_ID: string;
 }
 
 interface ImportMeta {
--- src/firebase/fbase.ts
+++ src/firebase/fbase.ts
@@ -3,24 +3,24 @@ import 'firebase/auth';
 import 'firebase/firestore';
 import 'firebase/storage';
 
-// const firebaseConfig = {
-//   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-//   authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-//   projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-//   storageBucket: process.env.REACT_APP_FIREBASE_BUCKET,
-//   messagingSenderId: process.env.REACT_APP_FIREBASE_SENDER_ID,
-//   appId: process.env.REACT_APP_FIREBASE_APP_ID,
-//   measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
-// };
+const {
+    VITE_FIREBASE_API_KEY,
+    VITE_FIREBASE_AUTH_DOMAIN,
+    VITE_FIREBASE_APP_ID,
+    VITE_FIREBASE_BUCKET,
+    VITE_FIREBASE_MEASUREMENT_ID,
+    VITE_FIREBASE_PROJECT_ID,
+    VITE_FIREBASE_SENDER_ID,
+} = import.meta.env;
 
 const firebaseConfig = {
-    apiKey: 'AIzaSyBHNA8PPt0Qwps_ZDJe-LpTfkuBR9m8OGY',
-    authDomain: 'onecoffee-e525f.firebaseapp.com',
-    projectId: 'onecoffee-e525f',
-    storageBucket: 'onecoffee-e525f.appspot.com',
-    messagingSenderId: '1076157403385',
-    appId: '1:1076157403385:web:87572d29545920fc65d228',
-    measurementId: 'G-96VVXT75DR',
+    apiKey: VITE_FIREBASE_API_KEY,
+    authDomain: VITE_FIREBASE_AUTH_DOMAIN,
+    projectId: VITE_FIREBASE_PROJECT_ID,
+    storageBucket: VITE_FIREBASE_BUCKET,
+    messagingSenderId: VITE_FIREBASE_SENDER_ID,
+    appId: VITE_FIREBASE_APP_ID,
+    measurementId: VITE_FIREBASE_MEASUREMENT_ID,
 };
 
 firebase.initializeApp(firebaseConfig);
