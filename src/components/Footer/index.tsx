d5059956b9fc491c2edaa4308529332e53f37343
zih0 <ziho@kakao.com>
2022-04-08 17:05:32 +0900

    feat: 푸터 메뉴 추가

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -12,11 +12,20 @@ const Container = styled.div`
     .wrapper {
         max-width: 1240px;
         width: 92%;
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
     }
 
-    .copyright {
+    .footer-text {
         font-size: 12px;
         font-weight: 300;
+    }
+
+    .contact-wrapper {
+        display: flex;
+        align-items: center;
+        gap: 0.5rem;
     }
 `;
 
@@ -24,7 +33,11 @@ function Footer() {
     return (
         <Container>
             <div className="wrapper">
-                <span className="copyright">2022 커피한잔</span>
+                <span className="footer-text">2022 커피한잔</span>
+                <div className="contact-wrapper">
+                    <span className="footer-text">about</span>|
+                    <div className="footer-text">feedback</div>
+                </div>
             </div>
         </Container>
     );
