fd957d871afa4907bbb340e5e58f240c2e1d21d3
zih0 <ziho@kakao.com>
2022-04-14 22:51:08 +0900

    feat: 모바일 감지 util 생성

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,9 +1,6 @@
-export const isMobile = () => {
-    const userAgent = navigator.userAgent;
+export const isMobile =
+    navigator.userAgent.match(
+        /iPhone|iPod|Android|Windows CE|BlackBerry|Symbian|Windows Phone|webOS|Opera Mini|Opera Mobi|POLARIS|IEMobile|lgtelecom|nokia|SonyEricsson/i,
+    ) != null || navigator.userAgent.match(/LG|SAMSUNG|Samsung/) !== null;
 
-    return (
-        userAgent.match(
-            /iPhone|iPod|Android|Windows CE|BlackBerry|Symbian|Windows Phone|webOS|Opera Mini|Opera Mobi|POLARIS|IEMobile|lgtelecom|nokia|SonyEricsson/i,
-        ) != null || userAgent.match(/LG|SAMSUNG|Samsung/) !== null
-    );
-};
+const isIos = /iPhone|iPad|iPod/i.test(navigator.userAgent);
